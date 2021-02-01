bc9496caa8ff8bf931dcca32ccfd37778f3ed1a7
Tanvir Shakil <tanvirshakil777@gmail.com>
2021-02-01 23:53:20 +0600

    js truthy and falsy values

---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+// truthy values :
+// 1, 2, 3, 4 etc;
+// ' ' (space string)
+// Infinity
+// {}
+// []
+// true
+
+// example
+let number = {}; // truthy value / falsy value
+if (number) {
+  console.log(`truthy value`);
+} else {
+  console.log(`falsy value`);
+}
+
+// falsy values :
+// 0 (only zero)
+// "" (no string)
+// undefined
+// NaN
+// null
+// false
--- truthyFalsy.js
+++
@@ -1,8 +0,0 @@
-// let count = "MyNameShakil";
-let count;
-console.log(count);
-if (count) {
-  console.log(`Condition True`);
-} else {
-  console.log(`Condition False`);
-}
