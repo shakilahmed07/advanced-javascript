f260a6d8d8b09e9745a3ed07e0342d1fe58a3466
Tanvir Shakil <tanvirshakil777@gmail.com>
2021-02-01 23:28:39 +0600

    js arrow function

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+// normal function
+function normal() {
+  return 25;
+}
+// console.log(normal());
+
+// arrow function
+const getNumber = () => {
+  return 2 + (5 * 20 * 68) / 5;
+};
+// console.log(getNumber());
