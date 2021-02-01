915b2d61b6ab92626c9b30dc18bf25874c6c824e
Tanvir Shakil <tanvirshakil777@gmail.com>
2021-02-01 23:35:23 +0600

    js spread array and object

--- spread.js
+++ spread.js
@@ -1,25 +1,15 @@
-// array spreading
+// array spread
 let number = [4, 5, 6, 11, 15, 49, 50];
 let newNumbers = [...number, 7, 8, 9, 10];
-
 // console.log(newNumbers);
-
-let sum = (a, d) => {
-  return a * d;
-};
-
-var numbers = [5, 6, 8, 10];
-// console.log(sum(...numbers));
 
 let calculated = (x, y) => {
   return x * y;
 };
-
 var arr = [8, 7];
 // console.log(calculated(...arr));
 
 // object spreading
-
 var myObj1 = {
   x: 5,
   y: 10,
@@ -31,5 +21,4 @@ var myObj2 = {
   b: 25,
   c: 30,
 };
-
-console.log({ ...myObj1, ...myObj2 });
+// console.log({ ...myObj1, ...myObj2 });
