5cabfceb286ec2d5636bd465d7e350e43f540229
tanvir-shakil <75533341+tanvir-shakil@users.noreply.github.com>
2021-02-02 18:47:36 +0600

    js arrow function

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+// normal function
+function normal() {
+  return 25;
+}
+console.log(normal()); // show 25
+
+// arrow function
+const getNumber = () => {
+  return 2 + (5 * 20 * 68) / 5;
+};
+console.log(getNumber()); // show 1362
