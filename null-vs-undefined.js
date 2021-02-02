ad2adacbc02b74192a1e58a67642be2875c560cb
Tanvir Shakil <tanvirshakil777@gmail.com>
2021-02-02 16:27:35 +0600

    js null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,13 @@
+// undefined
+// Declared as variable but no value set
+// example
+let example = undefined;
+console.log(example); // show undefined
+console.log(typeof example); // show undefined
+
+// null
+// Null is an assignment value. A defined variable representing a value
+// example
+let test = null;
+console.log(test); // show null
+console.log(typeof test); // show object
